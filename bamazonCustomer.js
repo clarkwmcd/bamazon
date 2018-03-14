9f6ec8c097cb9bd73cbac05bf123f78389d3778e
clarkwmcd <clarkwmcd@gmail.com>
2018-03-13 20:49:49 -0400

    fixed a bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,40 +5,62 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "root",
   database: "bamazon"
 });
 
-//CODE BELOW IS FROM CLASS 36 TOP SONGS CODE_JS.JS
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  runSearch();
+  displayProducts();
 });
 
-function runSearch() {
-  var query = "SELECT * FROM products";
+function displayProducts() {
   var list = [];
-
-  connection.query(query, function(err, res) {
+  connection.query("SELECT * FROM products", function(err, res) {
     console.log("|" + "Item" + "|" + "Department" + "|" + "Price" + "|" + "Quanity in Stock" + "|");
     for (var i = 0; i < res.length; i++) {
-      console.log("|" + res[i].product_name + "|" + res[i].department_name + "|" + res[i].price + "|" + res[i].stock_quanity + "|");
+      console.log("|" + res[i].product_name + "|" + res[i].department_name + "|" + res[i].price + "|" + res[i].stock_quantity + "|");
       list.push(res[i].product_name)
     }
+    runSearch(list);
+  });
+}
+
+function updateList(quantity, product) {
+  connection.query("UPDATE products SET stock_quantity=? WHERE product_name=?", [quantity, product], function (err, result) {
+    if (err) throw err;
+    console.log("database updated");
+  });
+}
+
+function runSearch(choices) {
+  inquirer.prompt({
+    type: 'list',
+    name: 'productlist',
+    message: 'What would you like to buy',
+    choices: choices
+  }).then(function(answers) {
+    var productselected = answers.productlist
+    console.log("You selected " + productselected);
     inquirer.prompt({
-      type: 'list',
-      name: 'productlist',
-      message: 'What would you like to buy',
-      choices: list
+      type: 'input',
+      name: 'quanity',
+      message: 'How many ' + productselected + 's would you like to buy?'
     }).then(function(answers) {
-      console.log("You selected " + answers.productlist);
-      if()
+      var quanity = parseInt(answers.quanity);
+      connection.query("SELECT stock_quantity FROM products WHERE product_name=?", productselected, function (err, result) {
+        if (err) throw err;
+        var stock = parseInt(result[0].stock_quantity);
+        if(quanity < stock) {
+          var newQuanity = stock - quanity;
+          console.log("We can fulfill your order of " + quanity + " " + productselected + "(s)");
+          updateList(newQuanity, productselected);
+        }
+        else {
+          console.log("Insufficient quantity!");
+        }
+      });
     });
   });
 };
